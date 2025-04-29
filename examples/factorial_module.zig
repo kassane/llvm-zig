6da284f664e50e7a7565110d7820170c3234cbae
Junyu Cao <190208351+junyucao1024@users.noreply.github.com>
2025-04-29 14:29:39 -0400

    fix the factorial example (#7)
    closes #6
    
    ---------
    
    Co-authored-by: junyucao1024 <junyucao1024@users.noreply.github.com>

--- examples/factorial_module.zig
+++ examples/factorial_module.zig
@@ -25,37 +25,83 @@ pub fn main() void {
     const factorialFunc: types.LLVMValueRef = core.LLVMAddFunction(module, "factorial", factorialFuncType);
 
     // Set up the function body
-    const entryBlock: types.LLVMBasicBlockRef = core.LLVMAppendBasicBlock(factorialFunc, "entry");
+    const entryBlock = core.LLVMAppendBasicBlock(factorialFunc, "entry");
+    const initBlock = core.LLVMAppendBasicBlock(factorialFunc, "init");
+    const condBlock = core.LLVMAppendBasicBlock(factorialFunc, "cond");
+    const bodyBlock = core.LLVMAppendBasicBlock(factorialFunc, "body");
+    const endBlock = core.LLVMAppendBasicBlock(factorialFunc, "end");
+
+    // Start entryBlock
     core.LLVMPositionBuilderAtEnd(builder, entryBlock);
+    const n = core.LLVMGetParam(factorialFunc, 0);
 
-    const n: types.LLVMValueRef = core.LLVMGetParam(factorialFunc, 0);
-    const loopBlock: types.LLVMBasicBlockRef = core.LLVMAppendBasicBlock(factorialFunc, "loop");
-    const endBlock: types.LLVMBasicBlockRef = core.LLVMAppendBasicBlock(factorialFunc, "end");
+    // allocate space to store values
+    // with pointers as return values 
+    const result_ptr = core.LLVMBuildAlloca(builder, returnType, "result_ptr");
+    const n_ptr = core.LLVMBuildAlloca(builder, returnType, "n_ptr");
+    // End entryBlock
 
-    _ = core.LLVMBuildBr(builder, loopBlock);
-    core.LLVMPositionBuilderAtEnd(builder, loopBlock);
-    const nIsZero: types.LLVMValueRef = core.LLVMBuildICmp(builder, types.LLVMIntPredicate.LLVMIntEQ, n, core.LLVMConstInt(core.LLVMInt32Type(), 0, 0), "nIsZero");
+    // Start initBlock, do some initialization
+    _ = core.LLVMBuildBr(builder, initBlock);
+    core.LLVMPositionBuilderAtEnd(builder, initBlock);
+    
+    // Set n_ptr.* = n, result.* = 1
+    _ = core.LLVMBuildStore(builder, core.LLVMConstInt(returnType, 1, 0), result_ptr);
+    _ = core.LLVMBuildStore(builder, n, n_ptr);
+    // End initBlock
 
-    _ = core.LLVMBuildCondBr(builder, nIsZero, endBlock, loopBlock);
+    // Start condBlock, set control flow
+    _ = core.LLVMBuildBr(builder, condBlock);
+    core.LLVMPositionBuilderAtEnd(builder, condBlock);
+
+    const n_val = core.LLVMBuildLoad2(builder, returnType, n_ptr, "n_val");
+    const isZero = core.LLVMBuildICmp(
+        builder,
+        types.LLVMIntPredicate.LLVMIntEQ,
+        n_val,
+        core.LLVMConstInt(returnType, 0, 0),
+        "n_is_zero",
+    );
+    _ = core.LLVMBuildCondBr(builder, isZero, endBlock, bodyBlock);
+    // If n_ptr.* = 0 then goto endBlock
+    // else goto bodyBlock
+    // End condBlock
+
+    // Start bodyBlock: 
+    // factorial *= n; n -= 1
+    core.LLVMPositionBuilderAtEnd(builder, bodyBlock);
+    const result_val = core.LLVMBuildLoad2(builder, returnType, result_ptr, "res");
+    const new_result = core.LLVMBuildMul(builder, result_val, n_val, "new_res");
+    _ = core.LLVMBuildStore(builder, new_result, result_ptr);
+
+    const new_n = core.LLVMBuildSub(
+        builder,
+        n_val,
+        core.LLVMConstInt(returnType, 1, 0),
+        "new_n",
+    );
+    _ = core.LLVMBuildStore(builder, new_n, n_ptr);
+    _ = core.LLVMBuildBr(builder, condBlock); 
+    // goto condBlock
+    // End bodyBlock
+
+    // Start endBlock
     core.LLVMPositionBuilderAtEnd(builder, endBlock);
-    const result: types.LLVMValueRef = core.LLVMBuildPhi(builder, returnType, "result");
-    const resultValue: types.LLVMValueRef = core.LLVMBuildPhi(builder, returnType, "result_value");
-    const resultPlusN: types.LLVMValueRef = core.LLVMBuildAdd(builder, resultValue, n, "result_plus_n");
-
-    _ = core.LLVMBuildStore(builder, resultPlusN, result);
-    const nIsOne: types.LLVMValueRef = core.LLVMBuildICmp(builder, types.LLVMIntPredicate.LLVMIntEQ, n, core.LLVMConstInt(core.LLVMInt32Type(), 1, 0), "nIsOne");
-    const newResult: types.LLVMValueRef = core.LLVMBuildSelect(builder, nIsOne, resultPlusN, resultValue, "newResult");
-
-    _ = core.LLVMBuildStore(builder, newResult, resultValue);
-    _ = core.LLVMBuildCondBr(builder, nIsOne, endBlock, loopBlock);
-
-    core.LLVMPositionBuilderAtEnd(builder, endBlock);
-    _ = core.LLVMBuildRet(builder, resultValue);
+    const final_result = core.LLVMBuildLoad2(builder, returnType, result_ptr, "final");
+    _ = core.LLVMBuildRet(builder, final_result);
+    // End endBlock
+    // Program ends
 
     // Verify the module
     var errMsg: ?[*:0]u8 = null;
-    _ = analysis.LLVMVerifyModule(module, types.LLVMVerifierFailureAction.LLVMPrintMessageAction, @ptrCast(&errMsg));
-    if (errMsg) |msg| {
+    const failed: types.LLVMBool = analysis.LLVMVerifyModule(
+        module,
+        types.LLVMVerifierFailureAction.LLVMReturnStatusAction,
+        @ptrCast(&errMsg),
+    );
+
+    if (failed != 0 and errMsg != null){ 
+        const msg: [*:0]u8 = errMsg.?;
         @panic(std.mem.span(msg));
         // core.LLVMDisposeMessage(msg);
         // std.os.exit(1);
